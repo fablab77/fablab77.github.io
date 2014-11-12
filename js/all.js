17cf0cfa552a752e68e82930648e861e4ee8b20a
Ksiusha5 <ksiusha5@mail.ru>
2014-11-12 17:55:34 +0200

    robotix

--- js/all.js
+++ js/all.js
@@ -5,4 +5,8 @@ $('document').ready(function(){
 		// console.log('hover');
 		// $('.fotorama').hover();
 	// });
+	$('.expo-slide a').click(function(e){
+		e.stopPropagation();
+	});
+	$('.fotorama .fotorama__nav__shaft .fotorama__thumb-border').next('.fotorama__nav__frame').find('.fotorama__thumb').append('<img src="img/slider/thumb/bot.jpg" width="100" height="68">');
 });
