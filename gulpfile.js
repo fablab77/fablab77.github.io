ffd0c3c64d66bdce0dd570553238c285cf4313d5
Eugen Soloviov <suenot@gmail.com>
2015-03-12 07:38:55 +0300

    fix gulpfile, fix main fotorama

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp'),
 	$ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+
+gulp.task('server', function() {
+	browserSync({
+		server: {
+			baseDir: "./"
+		},
+		browser: "google chrome"
+	});
+});
 
 gulp.task('sass', function () {
-	return gulp.src(['./sass/**/*.sass'])
+	return gulp.src(['./sass/**/*.sass', './sass/**/*.scss'])
 		.pipe($.sourcemaps.init())
 		.pipe($.sass({
 			indentedSyntax: true,
@@ -10,12 +20,12 @@ gulp.task('sass', function () {
 			outputStyle: 'compressed'
 		}))
 		.pipe($.autoprefixer('last 3 version'))
-		.pipe($.sourcemaps.write('.'))
+		// .pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch(['./sass/**/*.sass'], ['sass']);
+	gulp.watch(['./sass/**/*.sass', './sass/**/*.scss'], ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['server', 'sass', 'watch']);
