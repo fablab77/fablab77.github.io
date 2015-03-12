eeafe8989b10258f9e72be89f05acdb939ccedec
Eugen Soloviov <suenot@gmail.com>
2015-03-12 07:16:06 +0300

    test fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,21 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var connect = require('gulp-connect');
-var nib = require('nib');
-var chalk = require('chalk');
-var dateformat = require('dateformat');
-var fobject = require('fobject');
-var clean = require('gulp-clean');
-var coffee = require('gulp-coffee');
-var jade = require('gulp-jade');
-var uglify = require('gulp-uglify');
-var nib = require('nib');
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')();
 
-gulp.task('connect', function() {
-  connect.server({
-    port: 8080,
-    livereload: true
-  });
+gulp.task('sass', function () {
+	return gulp.src(['./sass/**/*.sass'])
+		.pipe($.sourcemaps.init())
+		.pipe($.sass({
+			indentedSyntax: true,
+			errLogToConsole: true,
+			outputStyle: 'compressed'
+		}))
+		.pipe($.autoprefixer('last 3 version'))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('css'));
 });
-
 
 gulp.task('watch', function () {
-  gulp.watch(['./css/**/*.styl'], ['stylus']);
-  gulp.watch(['./css/**/*.css','./index.html'], ['reload']);
+	gulp.watch(['./sass/**/*.sass'], ['sass']);
 });
 
-gulp.task('stylus', function () {
-  gulp.src('./css/**/*.styl')
-    .pipe(stylus({use: [nib()]}))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('reload', function () {
-    gulp.src(['./css/**/*.css','./index.html'])
-        .pipe(connect.reload());
-});
-
-
-// Default gulp task to run
-gulp.task('default', function(){
-    gulp.run('connect','stylus','watch');
-});
+gulp.task('default', ['sass', 'watch']);
