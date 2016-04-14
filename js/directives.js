f0632ec91a7243e373aaa3430c372834e58aeb5e
Eugen Soloviov <suenot@gmail.com>
2016-04-14 22:43:53 +0300

    + google tag manager

--- js/directives.js
+++ js/directives.js
@@ -37,6 +37,12 @@ app_d.directive('analytics', function($window){
 				d.addEventListener("DOMContentLoaded", f, false);
 			} else { f(); }
 		})(document, window, "yandex_metrika_callbacks");
+
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-MF2QM3');
 	};
 });
 
